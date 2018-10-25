76e0b5d4849d6ae915bc5f471b8c504354fcc4ee
DESKTOP-D3D299N\IGAWorks <jinh0park@naver.com>
2018-10-25 14:28:05 +0900

    지하철 데이터 저장하기

--- scripts/metroData/db.py
+++ scripts/metroData/db.py
@@ -1,4 +1,5 @@
 import django
+from django.db.utils import IntegrityError
 from django.conf import settings
 from metro.settings import DATABASES, ASSET_DIR
 from . import api
@@ -23,19 +24,33 @@ def update_metro_db(recent=False):
     try:
         with open(os.path.join(ASSET_DIR, 'seoul_metro_all.json'),'r',encoding='utf-8') as f:
             data = json.loads(f.read())
-            try:
-                for station in data['SearchSTNBySubwayLineService']['row']:
+            count = data['SearchSTNBySubwayLineService']['list_total_count']
+            c = 0
+            m = 0
+            for station in data['SearchSTNBySubwayLineService']['row']:
+                try:
                     Station.objects.create(
                         line_num= station['LINE_NUM'],
                         station_cd=station['STATION_CD'],
                         station_nm=station['STATION_NM'],
                         fr_code=station['FR_CODE']
                     )
-                print("DB Updated Successfully.")
-                return True
+                    c += 1
+                except IntegrityError:
+                    s = Station.objects.get(station_cd=station['STATION_CD'])
+                    if((s.line_num, s.station_nm, s.fr_code)
+                            == tuple([station[index] for index in ('LINE_NUM', 'STATION_NM', 'FR_CODE')])):
+                        pass
+                    else:
+                        s.line_num=station['LINE_NUM'],
+                        s.station_cd=station['STATION_CD'],
+                        s.station_nm=station['STATION_NM'],
+                        s.fr_code=station['FR_CODE']
+                        m += 1
 
-            except Exception as e:
-                print(e)
+
+            print("DB Updated.({} created / {} modified / {} total)".format(c, m, count))
+            return True
 
     except FileNotFoundError:
         print("There is no metro json file, Please update / download the file first.")
--- scripts/script.py
+++ scripts/script.py
@@ -2,4 +2,4 @@ from scripts.metroData import api as metroApi
 from scripts.metroData import db as metroDB
 
 #metroApi.update_metro_json_file()
-#metroDB.update_metro_db()
+metroDB.update_metro_db()
