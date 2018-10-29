e22b894cd20a39371cc40e00982bf8cb03bb45f1
DESKTOP-D3D299N\IGAWorks <jinh0park@naver.com>
2018-10-29 17:54:53 +0900

    User model import 문제 해결

--- seoul/models.py
+++ seoul/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -57,17 +57,17 @@ class Spot(models.Model):
 
 
 class CheckIn(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     spots = models.ManyToManyField(Spot, blank=True)
 
     def __str__(self):
         return self.user.username
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_checkin(sender,instance, created, **kwargs):
     if created:
         CheckIn.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_user_checkin(sender, instance, **kwargs):
     instance.checkin.save()
